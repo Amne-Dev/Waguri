69d47bf50f2d8f72bed5ac6c4211566df0959f71
AmDev <98409133+Amne-Dev@users.noreply.github.com>
2025-12-09 12:17:44 +0100

    Add audio unlock overlay for sound activation
    Introduces an overlay prompting users to click and enable sound, addressing browser autoplay restrictions. Updates CSS for overlay styling, adds the overlay to HTML, and handles its dismissal in sfx.js.

--- js/sfx.js
+++ js/sfx.js
@@ -2,6 +2,7 @@ const waguri = document.querySelector('.logo');
 const sfx = new Audio('sfx/unyah.mp3');
 sfx.loop = true
 sfx.playbackRate = 1.25
+sfx.volume = 0.4
 waguri.addEventListener('mouseenter', () => {
     sfx.currentTime = 0;
     sfx.play();
@@ -9,4 +10,9 @@ waguri.addEventListener('mouseenter', () => {
 
 waguri.addEventListener('mouseleave', () => {
     sfx.pause();
+});
+
+document.getElementById('audioUnlock').addEventListener('click', () => {
+  // This first click unlocks audio
+  document.getElementById('audioUnlock').style.display = 'none';
 });
